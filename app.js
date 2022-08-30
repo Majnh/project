9373982e017efbb809bd78f594b4720d7e5daa8b
Long Do <thanhlongtld@gmail.com>
2022-08-30 21:54:43 +0700

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "public/preview-saasto/sassto/index-2.html")
+  );
+});
+
+app.listen(4000, () => {
+  console.log("SWC is running on port 4000");
+});
